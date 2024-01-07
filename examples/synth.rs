127f246f0a499f73cc4206aee4a3576a4b2375b1
Wiktor W. <wykwit@disroot.org>
2024-01-07 16:52:20 +0100

    Update synth example

--- examples/synth.rs
+++ examples/synth.rs
@@ -1,24 +1,14 @@
-use muslib::algs::{synth, Algorithm, Variable};
-use muslib::mixer::Writer;
+use muslib::algs::*;
 
 fn main() {
-    let mut s = synth::new();
-
-    // params
-    s.envelope.update(Some(vec![0.1, 0.02, 0.2, 0.6, 0.1]));
-
-    // input
-    s.freq.update(Some(vec![440.0, 880.0, 660.0, 0.0])); // A_4, A_5, E_5, silence
-    s.durations.update(Some(vec![0.8, 0.7, 1.0, 0.5]));
+    let mut s = synth::Synthesizer::new();
+    s.envelope = vec![0.1, 0.02, 0.2, 0.6, 0.1];
+    s.freq = vec![440.0, 880.0, 660.0, 0.0]; // A_4, A_5, E_5, silence
+    s.durations = vec![0.8, 0.7, 1.0, 0.5];
     s.compute();
 
-    // output
-    let out = s.pcm_data.value().as_ref().unwrap();
-
-    // save
-    let mut w = Writer::new();
-    w.file("test.wav".into())
-        .sample_rate(44100)
-        .write(&out)
-        .unwrap();
+    let mut w = io::MonoWriter::new();
+    w.file = "test.wav".into();
+    w.pcm_data = s.pcm_data.unwrap();
+    w.compute()
 }
